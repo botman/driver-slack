f9e3f782f29afb2ca92833e96d261bc0c41087f8
Jérémy Touati <jeremy.touati@gmail.com>
2018-02-18 15:27:21 +0100

    Added json_encode return value safety check

--- src/SlackDriver.php
+++ src/SlackDriver.php
@@ -98,10 +98,13 @@ class SlackDriver extends HttpDriver implements VerifiesService
         // If the event type isn't 'message' (which should go through BotMan::hears),
         // build a GenericEvent and return it
         if (isset($eventData['type']) && $eventData['type'] !== 'message') {
-            $event = new GenericEvent(json_encode($eventData));
-            $event->setName($eventData['type']);
+            $eventPayload = json_encode($eventData);
+            if ($eventPayload !== false) {
+                $event = new GenericEvent($eventPayload);
+                $event->setName($eventData['type']);
 
-            return $event;
+                return $event;
+            }
         }
 
         // Otherwise, fall back to the parent implementation
